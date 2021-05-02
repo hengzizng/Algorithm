e56fbbf7c94d55e48b34f715f809464049ff9532
HYEJI <hengzizng@naver.com>
2021-05-03 00:16:23 +0900

    Solved 7576 - BaekJoon (BFS & DFS)

--- BaekJoon/7576.py
+++ BaekJoon/7576.py
@@ -4,35 +4,35 @@ from collections import deque
 read = sys.stdin.readline
 
 
-def bfs(start_positions):
-    queue = deque(start_positions)
+def make_ripe(riped_tomato, answer_info):
+    queue = deque(riped_tomato)
 
     while queue:
-        row, col, date = queue.popleft()
+        row, col, time = queue.popleft()
         answer_info[1] += 1
-
-        if row > 0 and box[row-1][col] == 0:
-            queue.append((row-1, col, date+1))
-            box[row-1][col] = 1
-        if row < N-1 and box[row+1][col] == 0:
-            queue.append((row+1, col, date+1))
-            box[row+1][col] = 1
-        if col > 0 and box[row][col-1] == 0:
-            queue.append((row, col-1, date+1))
-            box[row][col-1] = 1
-        if col < M-1 and box[row][col+1] == 0:
-            queue.append((row, col+1, date+1))
-            box[row][col+1] = 1
+        
+        if row > 0 and box[row - 1][col] == 0:
+            queue.append((row - 1, col, time + 1))
+            box[row - 1][col] = 1
+        if row < N - 1 and box[row + 1][col] == 0:
+            queue.append((row + 1, col, time + 1))
+            box[row + 1][col] = 1
+        if col > 0 and box[row][col - 1] == 0:
+            queue.append((row, col - 1, time + 1))
+            box[row][col - 1] = 1
+        if col < M - 1 and box[row][col + 1] == 0:
+            queue.append((row, col + 1, time + 1))
+            box[row][col + 1] = 1
     
-    return date
+    return time
 
 
 # M: 가로, N: 세로
 M, N = map(int, read().split())
 # box: 토마토 상태, riped_tomato: 익은 토마토들의 위치
 box, riped_tomato,  = [], []
-# answer_info: [토마토가 익기까지 최소 값, 체크한 위치 수]
-answer_info = [0, 0]
+# answer_info: [토마토가 익기까지 최소 날짜, 체크한 위치 수]
+answer_info = [-1, 0]
 for row in range(N):
     box.append(list(map(int, read().split())))
     for col in range(M):
@@ -41,8 +41,9 @@ for row in range(N):
         elif box[row][col] == -1:
             answer_info[1] += 1
 
-answer_info[0] = bfs(riped_tomato)
-if answer_info[1] == M * N:
-    print(answer_info[0])
-else:
-    print(-1)
+if riped_tomato:
+    answer_info[0] = make_ripe(riped_tomato, answer_info)
+    if answer_info[1] < M * N:
+        answer_info[0] = -1
+
+print(answer_info[0])
