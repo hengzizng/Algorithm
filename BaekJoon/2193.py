f061dbf9659ac4e57ae7134a471d11b96df49445
HYEJI <hengzizng@naver.com>
2021-06-10 12:52:34 +0900

    Resolved 2193 - BaekJoon (Dynamic Programming)

--- BaekJoon/2193.py
+++ BaekJoon/2193.py
@@ -1,3 +1,5 @@
+'''
+# Solution 1
 N = int(input())
 
 if N < 3:
@@ -11,3 +13,15 @@ else:
         d[i] = d[i - 1] + d[i - 2]
     
     print(d[N])
+'''
+
+# Solution 2 > 변수 세 개만 사용하는 것으로 수정
+N = int(input())
+
+x, y, z = 0, 1, 1
+
+for i in range(3, N + 1):
+    x, y = y, z
+    z = x + y
+
+print(z)
