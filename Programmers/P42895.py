e5a5f6780cb4c00ed8d31e7892206b87fb637164
HYEJI <hengzizng@naver.com>
2021-09-27 23:02:48 +0900

    Programmers 42895 코드 정리

--- Programmers/P42895.py
+++ Programmers/P42895.py
@@ -91,22 +91,13 @@ def solution(N, number):
 
 
 def solution(N, number):
-    if number == N:
-        return 1
-
     D = [set() for _ in range(9)] # N을 index 개수만큼 사용해서 만들 수 있는 수
     
-    D[1].add(N)
-    for count in range(2, 8 + 1): # N을 8개까지만 사용한다.
-        temp = N
-        for c in range(1, count):
-            temp += N * (10 ** c)
-        D[count].add(temp)
+    for count in range(1, 8 + 1): # N을 8개까지만 사용한다.
+        # NNN...N 형식의 연산
+        D[count].add(int(str(N) * count))
 
-        if temp == number:
-            return count
-
-        # N count개 사용 : part1 + part2
+        # N count개 사용 : part1개 + part2개
         # N 2개 사용 : 1개 + 1개
         # N 3개 사용 : 1개 + 2개
         # N 4개 사용 : 1개 + 3개, 2개 + 2개
@@ -118,27 +109,22 @@ def solution(N, number):
             part2 = count - part1
             for num1 in D[part1]: # part1개로 만들 수 있는 수들 탐색
                 for num2 in D[part2]: # part2개로 만들 수 있는 수들 탐색
-                    # number 만들 수 있으면 return
-                    if (
-                        num1 + num2 == number or num1 - num2 == number or num2 - num1 == number or
-                        num1 * num2 == number or num1 // num2 == number or num2 // num1 == number
-                        ):
-                        return count
-
                     # 덧셈 연산
                     D[count].add(num1 + num2)
                     # 뺄셈 연산
-                    if num1 - num2 > 0:
-                        D[count].add(num1 - num2)
-                    if num2 - num1 > 0:
-                        D[count].add(num2 - num1)
+                    D[count].add(num1 - num2)
+                    D[count].add(num2 - num1)
                     # 곱셈 연산
                     D[count].add(num1 * num2)
                     # 나눗셈 연산
-                    if num1 / num2 == num1 // num2:
+                    if num2 > 0 and num1 % num2 == 0:
                         D[count].add(num1 // num2)
-                    if num2 / num1 == num2 // num1:
+                    if num1 > 0 and num2 % num1 == 0:
                         D[count].add(num2 // num1)
+                    
+        # number 만들 수 있으면 return
+        if number in D[count]:
+            return count
 
     return -1
 
