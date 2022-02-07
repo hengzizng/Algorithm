0781497957dbb1724da6fbfe7e5d6c3139f89f25
hengzizng <hengzizng@naver.com>
2022-02-07 22:55:15 +0900

    Code Format 수정

--- BaekJoon/10026.py
+++ BaekJoon/10026.py
@@ -4,6 +4,7 @@ sys.setrecursionlimit(10**6)
 sys.stdin = open("TestCase/BaekJoon/10026input.txt")
 read = sys.stdin.readline
 
+
 def blueCheck(i, j):
     normal[i][j] = True
     abnormal[i][j] = True
@@ -14,6 +15,7 @@ def blueCheck(i, j):
         if 0 <= dx < N and 0 <= dy < N and not normal[dx][dy] and grid[dx][dy] == 'B':
             blueCheck(dx, dy)
 
+
 def abnormalCheck(i, j):
     abnormal[i][j] = True
 
@@ -23,6 +25,7 @@ def abnormalCheck(i, j):
         if 0 <= dx < N and 0 <= dy < N:
             if not abnormal[dx][dy] and grid[dx][dy] != 'B':
                 abnormalCheck(dx, dy)
+
 
 def normalCheck(i, j, origin_color):
     normal[i][j] = True
@@ -43,20 +46,20 @@ grid = []
 for _ in range(N):
     grid.append(list(read().strip()))
 
-count = [0, 0] # normal count, abnormal count
+count = [0, 0]  # normal count, abnormal count
 for i in range(N):
     for j in range(N):
-        if normal[i][j]: # 정상인 사람이 체크한 곳이면 색약인 사람도 체크했을 것
+        if normal[i][j]:  # 정상인 사람이 체크한 곳이면 색약인 사람도 체크했을 것
             continue
-        
-        if grid[i][j] == 'B': # 파란색은 정상, 색약인 모두 체크해야한다.
+
+        if grid[i][j] == 'B':  # 파란색은 정상, 색약인 모두 체크해야한다.
             blueCheck(i, j)
             count[0] += 1
             count[1] += 1
-        elif abnormal[i][j]: # 색약만 체크한 곳이면
+        elif abnormal[i][j]:  # 색약만 체크한 곳이면
             normalCheck(i, j, grid[i][j])
             count[0] += 1
-        else: # 파란색이 아닌데 색약이 체크하지 않은 곳이면
+        else:  # 파란색이 아닌데 색약이 체크하지 않은 곳이면
             normalCheck(i, j, grid[i][j])
             abnormalCheck(i, j)
             count[0] += 1
