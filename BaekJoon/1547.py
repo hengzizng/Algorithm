b653a5cc2b083132ad785afec8329bfd7eebbdf4
hengzizng <hengzizng@naver.com>
2022-02-21 23:53:23 +0900

    Solved 1547 - BaekJoon (Simulation)

---
+++ BaekJoon/1547.py
@@ -0,0 +1,17 @@
+from re import M
+import sys
+read = sys.stdin.readline
+
+cups = list(range(4))
+
+# M: 컵의 위치를 바꾼 횟수
+M = int(read())
+for _ in range(M):
+    cup1, cup2 = map(int, read().split())
+    cups[cup1], cups[cup2] = cups[cup2], cups[cup1]
+    print(cups)
+
+for no in range(1, 3 + 1):
+    if cups[no] == 1:
+        print(no)
+        break
