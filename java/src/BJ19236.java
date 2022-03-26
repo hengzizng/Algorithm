abf783f4084aa9b6676dda976d038994154da85c
hengzizng <hengzizng@naver.com>
2022-03-27 00:58:26 +0900

    Edit 19238 - BaekJoon (Simulation)

--- java/src/BJ19236.java
+++ java/src/BJ19236.java
@@ -19,17 +19,15 @@ public class BJ19236 {
         ////// test
 
         int[][] board = new int[SIZE][SIZE]; // 각 위치별 물고기 번호 (물고기 없으면 0)
-        int[][] fishes = new int[SIZE * SIZE + 1][3]; // 물고기 번호별 위치, 방향 (물고기 없으면 -1)
-        for (int fishNo = 1; fishNo <= SIZE * SIZE; fishNo++) {
-            for (int i = 0; i < 3; i++) {
-                fishes[fishNo][i] = -1;
-            }
+        int[][] fishes = new int[SIZE * SIZE][3]; // 물고기 번호별 위치, 방향 (물고기 없으면 -1)
+        for (int fishNo = 0; fishNo < SIZE * SIZE; fishNo++) {
+            fishes[fishNo][0] = -1;
         }
 
         for (int r = 0; r < SIZE; r++) {
             st = new StringTokenizer(in.readLine());
             for (int c = 0; c < SIZE; c++) {
-                int fishNo = Integer.parseInt(st.nextToken());
+                int fishNo = Integer.parseInt(st.nextToken()) - 1;
                 int fishDirection = Integer.parseInt(st.nextToken()) - 1;
 
                 board[r][c] = fishNo;
@@ -42,50 +40,85 @@ public class BJ19236 {
         // 상어 (0,0)에 들어감
         int r = 0, c = 0;
         int fishNo = board[r][c];
-        int[] fish = fishes[fishNo];
-        board[r][c] = 0;
+        board[r][c] = -1;
         fishes[fishNo][0] = -1;
 
         // 물고기 이동 -> 상어 이동 -> 물고기 이동 ...
-        moveFish(r, c, board, fishes);
-        moveAll(r, c, fish[2], fishNo, board, fishes);
+        moveAll(r, c, fishes[fishNo][2], fishNo + 1, board, fishes);
 
         // 상어가 먹을 수 있는 물고기 번호 합의 최댓값
         System.out.println(maxSum);
     }
 
     public static void moveAll(int r, int c, int d, int eatSum, int[][] board, int[][] fishes) {
-        int nr, nc, nd, fishNo;
-        boolean canMove = false;
+        // 물고기가 이동
+        moveFish(r, c, board, fishes);
 
+        int nr, nc, nd, fishNo;
         for (int i = 1; i < SIZE; i++) {
             // 상어의 이동: 방향에 있는 모든 칸으로 이동 가능 (한 번에 여러 칸 이동 가능)
             nr = r + (dr[d] * i);
             nc = c + (dc[d] * i);
             // 이동 가능하고 물고기가 있는 칸으로만 이동 가능
-            if (isValid(nr, nc) && board[nr][nc] > 0) {
+            if (isValid(nr, nc) && board[nr][nc] >= 0) {
                 int[][] newBoard = getCopied(board);
                 int[][] newFishes = getCopied(fishes);
 
                 // 이동해서 물고기를 먹음
                 fishNo = newBoard[nr][nc];
                 nd = newFishes[fishNo][2];
-                newBoard[nr][nc] = 0;
+                newBoard[nr][nc] = -1;
                 newFishes[fishNo][0] = -1;
-                canMove = true;
-
-                // 물고기가 이동
-                moveFish(nr, nc, newBoard, newFishes);
 
                 // 다음 위치 탐색
-                moveAll(nr, nc, nd, eatSum + fishNo, newBoard, newFishes);
+                moveAll(nr, nc, nd, eatSum + (fishNo + 1), newBoard, newFishes);
             }
         }
 
-        // 상어가 이동할 곳이 없다면 종료
-        if (!canMove) {
-            maxSum = Math.max(maxSum, eatSum);
-            return;
+        // 매번 최댓값 갱신
+        maxSum = Math.max(maxSum, eatSum);
+    }
+
+    // 물고기의 이동
+    public static void moveFish(int sharkR, int sharkC, int[][] board, int[][] fishes) {
+        int r, c, d, nr = -1, nc = -1, nd, target;
+        for (int fishNo = 0; fishNo < SIZE * SIZE; fishNo++) { // 물고기 번호 오름차순으로 확인
+            if (fishes[fishNo][0] == -1) { // 물고기 번호 fishNo인 물고기가 없다면
+                continue;
+            }
+
+            // 이번 물고기의 원래 위치, 방향 정보
+            r = fishes[fishNo][0];
+            c = fishes[fishNo][1];
+            d = fishes[fishNo][2];
+
+            nd = d;
+            for (int i = 0; i < 8; i++) {
+                nr = r + dr[nd];
+                nc = c + dc[nd];
+
+                // 이동 가능한 방향이 없다면 반시계 방향으로 45도 회전
+                if (!isValid(nr, nc) || (nr == sharkR && nc == sharkC)) {
+                    nd = (nd + 1) % 8;
+                    continue;
+                }
+
+                // 이동 가능한 방향이 있다면 (빈 칸, 다른 물고기가 있는 칸)
+                target = board[nr][nc]; // 위치를 교환할 물고기
+
+                // 물고기 이동 (서로 위치 교환)
+                fishes[fishNo][0] = nr;
+                fishes[fishNo][1] = nc;
+                fishes[fishNo][2] = nd;
+                if (target >= 0) {
+                    fishes[target][0] = r;
+                    fishes[target][1] = c;
+                }
+
+                board[nr][nc] = fishNo;
+                board[r][c] = target;
+                break;
+            }
         }
     }
 
@@ -100,53 +133,6 @@ public class BJ19236 {
         }
 
         return copied;
-    }
-
-    // 물고기의 이동
-    public static void moveFish(int sharkR, int sharkC, int[][] board, int[][] fishes) {
-        int r, c, d, nr = -1, nc = -1, nd, target;
-        for (int fishNo = 1; fishNo <= SIZE * SIZE; fishNo++) { // 물고기 번호 오름차순으로 확인
-            if (fishes[fishNo][0] == -1) { // 물고기 번호 fishNo인 물고기가 없다면
-                continue;
-            }
-
-            // 이번 물고기의 원래 위치, 방향 정보
-            int[] fish = fishes[fishNo]; // {행, 열, 방향}
-            r = fish[0];
-            c = fish[1];
-            d = fish[2];
-
-            boolean canMove = false; // 이동 가능 여부
-            nd = d;
-            for (int i = 0; i < 8; i++) {
-                nr = r + dr[nd];
-                nc = c + dc[nd];
-
-                // 이동 가능한 방향이 있다면 (빈 칸, 다른 물고기가 있는 칸)
-                if (isValid(nr, nc) && (nr != sharkR || nc != sharkC)) {
-                    canMove = true;
-                    break;
-                }
-                // 이동 가능한 방향이 없다면 반시계 방향으로 45도 회전
-                nd = (nd + 1) % 8;
-            }
-
-            // 물고기 이동 (서로 위치 교환)
-            if (canMove) {
-                target = board[nr][nc]; // 위치를 교환할 물고기
-
-                fishes[fishNo][0] = nr;
-                fishes[fishNo][1] = nc;
-                fishes[fishNo][2] = nd;
-                if (target > 0) {
-                    fishes[target][0] = r;
-                    fishes[target][1] = c;
-                }
-
-                board[nr][nc] = fishNo;
-                board[r][c] = target;
-            }
-        }
     }
 
     // 공간 안에 있는지 여부 반환
