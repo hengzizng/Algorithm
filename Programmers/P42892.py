a68deb5cda4e1aaaf4e9f8dea50291a2f96111b7
hengzizng <hengzizng@naver.com>
2022-09-20 16:45:53 +0900

    93.1

--- Programmers/P42892.py
+++ Programmers/P42892.py
@@ -2,18 +2,7 @@ from collections import defaultdict
 
 
 def solution(nodeinfo):
-    def search(node):
-        if node == 0:
-            return
-
-        # 전위 순회: 부모 -> 왼쪽 자식 -> 오른쪽 자식
-        # 후위 순회: 왼쪽 자식 -> 오른쪽 자식 -> 부모
-        result[0].append(node)
-        search(tree[node][0])  # 왼쪽 자식
-        search(tree[node][1])  # 오른쪽 자식
-        result[1].append(node)
-
-    def make_tree(x_idx, y_idx, min_x, max_x):
+    def search(x_idx, y_idx, min_x, max_x):
         y = y_list[y_idx]
         x, node = x_by_y[y][x_idx]
 
@@ -21,20 +10,24 @@ def solution(nodeinfo):
         if x < min_x or x > max_x:
             return False
 
-        # 이번 노드가 리프 노드인 경우
-        if y_idx + 1 == len(y_list):
-            return True
+        # 전위 순회: 부모 -> 왼쪽 자식 -> 오른쪽 자식
+        result[0].append(node)
 
-        child_y = y_list[y_idx + 1]  # 이번 노드의 자식 y좌표
-        for child_x_idx in range(len(x_by_y[child_y])):
-            child_node = x_by_y[child_y][child_x_idx][1]
+        # 이번 노드가 리프 노드가 아닌 경우
+        if y_idx + 1 < len(y_list):
+            child_y = y_list[y_idx + 1]  # 이번 노드의 자식 y좌표
+            for child_x_idx in range(len(x_by_y[child_y])):
+                child_node = x_by_y[child_y][child_x_idx][1]
 
-            # 왼쪽 서브 트리
-            if make_tree(child_x_idx, y_idx + 1, min_x, x - 1):
-                tree[node][0] = child_node
-            # 오른쪽 서브 트리
-            elif make_tree(child_x_idx, y_idx + 1, x + 1, max_x):
-                tree[node][1] = child_node
+                # 왼쪽 서브 트리
+                if search(child_x_idx, y_idx + 1, min_x, x - 1):
+                    tree[node][0] = child_node
+                # 오른쪽 서브 트리
+                elif search(child_x_idx, y_idx + 1, x + 1, max_x):
+                    tree[node][1] = child_node
+
+        # 후위 순회: 왼쪽 자식 -> 오른쪽 자식 -> 부모
+        result[1].append(node)
 
         return True
 
@@ -50,13 +43,9 @@ def solution(nodeinfo):
     for y in y_list:
         x_by_y[y].sort()  # 각 y좌표별로 x좌표 오름차순 정렬
 
-    # 트리를 만든다
-    make_tree(0, 0, 0, 100000)
-
-    # 루트 노드부터 탐색 시작
+    # 탐색 시작
     result = [[], []]
-    root = x_by_y[y_list[0]][0][1]
-    search(root)
+    search(0, 0, 0, 100000)
 
     return result
 
