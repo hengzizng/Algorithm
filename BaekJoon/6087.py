b072ded4429de31b9f2a6a21f0ba0b7fa46c8329
hengzizng <hengzizng@naver.com>
2023-03-31 21:07:54 +0900

    Solved 6087 - BaekJoon (BFS & DFS)

--- BaekJoon/6087.py
+++ BaekJoon/6087.py
@@ -1,44 +1,60 @@
-from collections import deque
+from heapq import *
 import sys
+
 read = sys.stdin.readline
 
 
-drdc = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # 상하좌우
-W, H = map(int, read().split())  # 너비, 높이
-board = [list(read().strip()) for _ in range(H)]  # 지도
-visited = [[W * H] * W for _ in range(H)]  # 각 칸에 도착하려면 필요한 거울의 최솟값
+def is_valid(r, c):
+    if 0 <= r < H and 0 <= c < W and board[r][c] != '*':
+        return True
+    return False
 
-# 'C' 로 표시되어 있는 칸을 찾는다.
-depart, dest = [-1, -1], [-1, -1]
+
+# 상, 우, 하, 좌
+drdc = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+# W: 너비, H: 높이
+W, H = map(int, read().split())
+# board 값: . 빈칸, * 벽, C 레이저로 연결할 두 칸
+board = [list(read().strip()) for _ in range(H)]
+# targets: 레이저로 연결할 두 칸의 행, 열 좌표
+targets = []
 for r in range(H):
     for c in range(W):
         if board[r][c] == 'C':
-            if depart[0] == -1:
-                depart = [r, c]
-            else:
-                dest = [r, c]
+            targets.append([r, c])
 
+# turn_counts[r][c][d]: 방향별로 target 1부터 각 위치까지 경로들 중 최소 방향전환 횟수
+turn_counts = [[[W * H] * 4 for _ in range(W)] for _ in range(H)]
 
-queue = deque([(depart[0], depart[1])])  # bfs를 위한 큐 (행, 열)
-visited[depart[0]][depart[1]] = 0
+# (방향전환 횟수, 행, 열, 방향)
+pq = [(0, *targets[0], 0), (0, *targets[0], 1),
+      (0, *targets[0], 2), (0, *targets[0], 3)]
+while pq:
+    turn_count, r, c, d = heappop(pq)
 
-while queue:
-    r, c = queue.popleft()
+    if turn_count >= turn_counts[r][c][d]:
+        continue
 
-    for nd in range(4):
-        nr, nc = r + drdc[nd][0], c + drdc[nd][1]
+    turn_counts[r][c][d] = turn_count
 
-        # 거울을 더 추가하지 않고 이동할 수 있는 만큼 이동
-        while 0 <= nr < H and 0 <= nc < W and board[nr][nc] != '*':
-            # 가려는 위치에 더 적은 거울로도 이동 가능하다면
-            if visited[r][c] + 1 > visited[nr][nc]:
-                break
+    if r == targets[1][0] and c == targets[1][1]:
+        break
 
-            queue.append((nr, nc))
-            visited[nr][nc] = visited[r][c] + 1
+    # 현재 진행방향으로 이동
+    nr, nc = r + drdc[d][0], c + drdc[d][1]
+    if is_valid(nr, nc):
+        heappush(pq, (turn_count, nr, nc, d))
 
-            nr += drdc[nd][0]
-            nc += drdc[nd][1]
+    # 시계 방향으로 90도 회전해서 이동
+    nd = (d + 1) % 4
+    nr, nc = r + drdc[nd][0], c + drdc[nd][1]
+    if is_valid(nr, nc):
+        heappush(pq, (turn_count + 1, nr, nc, nd))
 
-# 'C'에서는 어떤 방향으로도 이동할 수 있기 때문에 처음에 더한 1 값을 빼준다
-print(visited[dest[0]][dest[1]] - 1)
+    # 반시계 방향으로 90도 회전해서 이동
+    nd = (d + 3) % 4
+    nr, nc = r + drdc[nd][0], c + drdc[nd][1]
+    if is_valid(nr, nc):
+        heappush(pq, (turn_count + 1, nr, nc, nd))
+
+print(min(turn_counts[targets[1][0]][targets[1][1]]))
